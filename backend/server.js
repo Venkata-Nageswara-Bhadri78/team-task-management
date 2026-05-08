2ed17a70fb0cea954cd1797c326bef9d2947cb81
Bhadri Venkata Nageswara <venkatanageswarabhadri12@gmail.com>
2026-05-08 15:18:42 +0530

    Added Few changes for Deployment

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/dashboard', dashboardRoutes);
 
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (err) => {
   if (err) {
